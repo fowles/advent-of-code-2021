09ee97d5d2f59562ca65ca9c6d5089635983e039
Matt Kulukundis <matt.fowles@gmail.com>
2021-12-04 14:18:36 -0500

    day1 part1 solution

--- aoc-2021/main.m
+++ aoc-2021/main.m
@@ -7,10 +7,32 @@
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+NSString* readFile(NSString* path) {
+  return [[NSString stringWithContentsOfFile:@"input/day1.txt"
+                                    encoding:NSUTF8StringEncoding
+                                       error:NULL]
+      stringByTrimmingCharactersInSet:NSCharacterSet
+                                          .whitespaceAndNewlineCharacterSet];
+}
+
+NSArray *splitLines(NSString *s) { return [s componentsSeparatedByString:@"\n"]; }
+
+int main(int argc, const char *argv[]) {
+  @autoreleasepool {
+    id lines = splitLines(readFile(@"input/day1.txt"));
+    id iter = [lines objectEnumerator];
+
+    NSString* last = [iter nextObject];
+    NSString* next;
+    int increasing = 0;
+    while (next = [iter nextObject]) {
+      if (last.intValue < next.intValue) {
+        ++increasing;
+      }
+      last = next;
     }
-    return 0;
+
+    NSLog(@"%d", increasing);
+  }
+  return 0;
 }
