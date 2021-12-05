8e8d26d33d1e3b3e38d1929e9d70d794f9f6f038
Matt Kulukundis <matt.fowles@gmail.com>
2021-12-04 23:42:37 -0500

    Set the working dir explicitly
    Use the bazel environment variable to set the working dir so the input
    files be found with relative paths.

--- main.m
+++ main.m
@@ -1,11 +1,14 @@
 #import <Foundation/Foundation.h>
 
+#include <stdlib.h>
+
 #import "day1.h"
 #import "day2.h"
 #import "day3.h"
 #import "parsing.h"
 
 int main(int argc, const char** argv) {
+  chdir(getenv("BUILD_WORKING_DIRECTORY"));
   @autoreleasepool {
     return day3main(argc, argv);
   }
