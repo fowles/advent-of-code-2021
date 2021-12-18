99c81c85611b9608079ddf026c20c5264ed3acc2
Matt Kulukundis <matt.fowles@gmail.com>
2021-12-17 19:28:21 -0500

    Day 6 part 2
    Fuck efficiency was a pretty ironic thing to write for part 1.  I
    guess I deserved that.

--- day6.m
+++ day6.m
@@ -3,27 +3,42 @@
 #import "day6.h"
 #import "parsing.h"
 
-void AdvanceDay(NSMutableArray<NSString*>* fish) {
-  NSUInteger s = fish.count;
-  for (NSUInteger i = 0; i < s; ++i) {
-    if ([fish[i] intValue] == 0) {
-      fish[i] = @"6";
-      [fish addObject: @"8"];
-    } else {
-      fish[i] = @([fish[i] intValue] - 1).stringValue;
-    }
+void AgeFish(size_t* fish_ages) {
+  size_t pregers = fish_ages[0];
+  for (int i = 1; i < 9; ++i) {
+    fish_ages[i-1] = fish_ages[i];
   }
+  fish_ages[8] = pregers;
+  fish_ages[6] += pregers;
 }
 
-NSInteger day6part1(NSMutableArray<NSString*>* nums) {
+size_t CountFish(size_t* fish_ages) {
+  size_t res = 0;
+  for (int i = 0; i < 9; ++i) res += fish_ages[i];
+  return res;
+}
+
+size_t day6part1(size_t* fish_ages) {
   for (int i = 0; i < 80; ++i) {
-    AdvanceDay(nums);
+    AgeFish(fish_ages);
   }
-  return nums.count;
+  return CountFish(fish_ages);
+}
+
+size_t day6part2(size_t* fish_ages) {
+  for (int i = 80; i < 256; ++i) {
+    AgeFish(fish_ages);
+  }
+  return CountFish(fish_ages);
 }
 
 int day6main(int argc, const char** argv) {
   id nums = split(readFile(@"input/day6.txt"), @",");
-  NSLog(@"Part 1: %ld", day6part1(nums));
+  size_t fish_ages[9] = {};
+  for (id n in nums) {
+    ++fish_ages[[n intValue]];
+  }
+  NSLog(@"Part 1: %ld", day6part1(fish_ages));
+  NSLog(@"Part 2: %ld", day6part2(fish_ages));
   return 0;
 }
